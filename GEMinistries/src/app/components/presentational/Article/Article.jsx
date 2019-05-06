0c8275aa4debfaccc5b575bef660acad97e51832
Mike Isome <misome@clearc2.com>
2019-05-06 17:28:08 -0500

    build(article-overflow): add overflow functionality to article component

--- GEMinistries/src/app/components/presentational/Article/Article.jsx
+++ GEMinistries/src/app/components/presentational/Article/Article.jsx
@@ -3,17 +3,32 @@ import PropTypes from 'prop-types';
 
 import './style.css';
 
-const Article = ({header, summary, width, overflows, articleLink}) => (
-  <article id="" style={{width : width}}>
+const Article = ({header, summary, summaryid, width, overflows, articleLink}) => (
+  <article style={{width : width}}>
     <div className="article-summary">
       <h4>{header}</h4>
-      {summary.split(/\n/g).map((paragraph, index) => {
-        const key = `${header}-${index}`
-        return (
-          paragraph
-          && <p key={key}>{paragraph}</p>)
-      })}
-      {overflows && <a href={articleLink}> Read More... </a>}
+      <div id={summaryid} >
+        {summary.split(/\n/g).map((paragraph, index) => {
+          const key = `${header}-${index}`
+          return (
+            paragraph
+            && <p key={key}>{paragraph}</p>)
+        })}
+      </div>
+      {overflows.doesOverflow &&
+        <span
+          className='article-link'
+          onClick={e => {
+
+             if (articleLink)
+               articleLink(summaryid)
+             const more = '... Read More'
+             const less = 'Read Less...'
+             e.target.text = e.target.text === more ? less : more
+           }}>
+          Read More...
+        </span>
+      }
     </div>
     <hr/>
   </article>
@@ -22,9 +37,10 @@ const Article = ({header, summary, width, overflows, articleLink}) => (
 Article.propTypes = {
   header: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
   width: PropTypes.number,
   overflows: PropTypes.bool,
-  articleLink: PropTypes.string
+  articleLink: PropTypes.func
 }
 
 export default Article;
